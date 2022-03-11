a0bedba8dadf9e0dc838ea14928377f718fad727
Defolt163 <61579315+Defolt163@users.noreply.github.com>
2022-03-11 17:51:42 +0400

    added load more card

--- js/script.js
+++ js/script.js
@@ -6,13 +6,23 @@ const menu = document.querySelector(".menu_list"),
       humburger.addEventListener('click', () =>{
           humburger.classList.toggle("humburger-active");
           menu.classList.toggle('menu_list-active');
-          fixBody.classList.toggle('body-fixed')
+          fixBody.classList.toggle('body-fixed');
       });
 
       menuItem.forEach(item =>{
           item.addEventListener('click', () =>{
             humburger.classList.toggle("humburger-active");
             menu.classList.toggle('menu_list-active');
+            fixBody.classList.toggle('body-fixed');
           })
       });
       
+const showMore = document.getElementById('scenarios-more'),
+      hiddensCard = document.querySelector(".scenarios__card_block-hidden"),
+      refreshOffers = document.querySelector(".offers");
+
+      showMore.addEventListener('click', () =>{
+          hiddensCard.classList.remove('scenarios__card_block-hidden');
+          showMore.style.display = 'none';
+          refreshOffers.style.margin= '20px 0px 0px 0px';
+      });
